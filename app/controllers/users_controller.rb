c19eb2c64adb46314bd7fbc04a09e5d0a919883d
Gonta-prfm <k.0426.s.46@icloud.com>
2021-02-18 18:14:40 +0900

    users_controller.rbの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
   def show
     @user = User.find(params[:id])
     # @user_name = current_user.name
-    @prototypes = current_user.prototypes
+    @prototypes = @user.prototypes
   end
 end
